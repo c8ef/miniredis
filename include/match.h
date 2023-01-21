efc8e1bf9c10e4eecc9c2f8d160a2cf26874b9f0
c8ef <c8ef@outlook.com>
2023-01-21 21:58:37 +0800

    add some comment

--- include/match.h
+++ include/match.h
@@ -2,4 +2,8 @@
 
 #include <stdbool.h>
 
+// '*'     matches any sequence of non-separator characters
+// '?'     matches any single non-separator
+// c       matches character c (c != '*', '?')
+// '\\' c  matches character c
 bool match(const char* pat, long plen, const char* str, long slen);
